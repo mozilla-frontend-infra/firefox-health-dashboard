85660292b55d1bc3f3aac038a425c6bcf63e99ce
Harald Kirschner <hkirschner@mozilla.com>
2016-07-08 12:16:51 -0700

    Skip crash-stats tests on travis

--- test/crashes/adi.js
+++ test/crashes/adi.js
@@ -3,20 +3,22 @@ import assert from 'assert';
 import { getAdi } from '../../src/crashes/adi';
 
 describe('getAdi', () => {
-  it('should return an array for Firefox', async () => {
-    const data = await getAdi({
-      dateRange: [new Date('2016-01-01'), new Date('2016-01-30')],
+  if (process.env.CRASH_STATS_TOKEN) {
+    it('should return an array for Firefox', async () => {
+      const data = await getAdi({
+        dateRange: [new Date('2016-01-01'), new Date('2016-01-30')],
+      });
+      assert(Array.isArray(data));
+      assert(data.length > 1);
     });
-    assert(Array.isArray(data));
-    assert(data.length > 1);
-  });
 
-  it('should return an array for Fennec', async () => {
-    const data = await getAdi({
-      dateRange: [new Date('2016-01-01'), new Date('2016-01-30')],
-      product: 'fennec',
+    it('should return an array for Fennec', async () => {
+      const data = await getAdi({
+        dateRange: [new Date('2016-01-01'), new Date('2016-01-30')],
+        product: 'fennec',
+      });
+      assert(Array.isArray(data));
+      assert(data.length > 1);
     });
-    assert(Array.isArray(data));
-    assert(data.length > 1);
-  });
+  }
 });
