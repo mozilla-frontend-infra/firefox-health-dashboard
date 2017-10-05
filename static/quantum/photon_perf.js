d25b10ba5771d638429217153038b1939f0db955
Armen Zambrano G <armenzg@gmail.com>
2017-10-05 16:12:03 -0400

    Possibly throw away code

--- static/quantum/photon_perf.js
+++ static/quantum/photon_perf.js
@@ -1,32 +1,48 @@
+/* global fetch */
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import Benchmark from './benchmark';
 
 
-const TelemetryPlotContainer = ({ evolutions }) => (
-  <div>
-    {evolutions.map(({ evo }) => (
-      <div key={evo.measure}>
-        <div>{evo}</div>
-      </div>
-    ))}
-  </div>
-);
-
-TelemetryPlotContainer.propTypes = {
-  evolutions: PropTypes.array,
-};
+// const TelemetryPlotContainer = ({ evolutions }) => (
+//   <div>
+//     {evolutions.map(({ evo }) => (
+//       <div key={evo.measure}>
+//         <div>{evo}</div>
+//       </div>
+//     ))}
+//   </div>
+// );
+//
+// TelemetryPlotContainer.propTypes = {
+//   evolutions: PropTypes.array,
+// };
 
 export default class PhotonPerfContainer extends React.Component {
-  state = {
-    evolutions: [],
+  constructor(props) {
+    super(props);
+    this.fetchEvolutions();
   }
-  componentDidMount() {
-    // TODO: Fetch /api/perf/photon
+
+  async fetchEvolutions() {
+    const evolutions = await (await fetch('/api/perf/notes')).json();
+    console.log(evolutions);
+    this.setState({
+      evolutions,
+    });
   }
 
   render() {
     const { evolutions } = this.state;
 
-    return (<TelemetryPlotContainer evolutions={evolutions} />);
+    return (
+      <Benchmark
+        title='Photon performance benchmark'
+        id='photon-perf'
+        link='http://astithas.com/perm/photon-perf/'
+        targetDiff={20}
+        type='line'
+      />
+    );
   }
 }
