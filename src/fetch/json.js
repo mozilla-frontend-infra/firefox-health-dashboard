d053f53bf07c0b9ee553475b920f47419796c403
Armen Zambrano G <armenzg@gmail.com>
2018-01-31 11:01:41 -0500

    Remove backend and switch to Neutrino

--- src/fetch/json.js
+++
@@ -1,9 +0,0 @@
-import fetchText from './text';
-
-export default async function fetchJson(url, options) {
-  const text = await fetchText(url, options);
-  if (!text) {
-    return null;
-  }
-  return JSON.parse(text);
-}
--- src/release/channels.js
+++
@@ -1,1 +0,0 @@
-export default ['release', 'beta', 'aurora', 'nightly'];
