815981dcdd910bb96c3d8db41cd20c2ac0a267f9
Armen Zambrano G <armenzg@gmail.com>
2018-02-21 10:06:53 -0500

    More changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { AppContainer } from 'react-hot-loader';
 import './index.css';
 import Routes from './routes';
 
+console.log(process.env.NODE_ENV);
+
 const root = document.getElementById('root');
 const load = () => render(
   (
