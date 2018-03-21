e8f78b317ef5f6b956045e46d6dbc5b977c8992b
Armen Zambrano G <armenzg@gmail.com>
2018-03-21 12:53:38 -0400

    Add initial test

---
+++ test/utils/subbenchmarks.test.js
@@ -0,0 +1,19 @@
+/* global describe, it */
+import assert from 'assert';
+import { expect } from 'chai';
+import { adjustedData } from '../../src/utils/perfherder/subbenchmarks';
+
+const stubData = [
+  { push_timestamp: 1518774673, value: 1 },
+  { push_timestamp: 1518774673, value: 3 },
+  { push_timestamp: 1518774673, value: 2 },
+  { push_timestamp: 1518774673, value: 4 },
+];
+
+describe('subbenchmarks', () => {
+  it('adjustData() - only keep 50th percentile of data', () => {
+    const data = adjustedData(stubData, 50);
+    assert(data.length === 2);
+    expect(data.map(d => d.value)).to.have.same.members([1, 2]);
+  });
+});
