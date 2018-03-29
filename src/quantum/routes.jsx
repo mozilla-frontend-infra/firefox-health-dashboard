2f586223e779edb799cf7e7990d3f0883c00edc0
Sarah Clements <sclements313@gmail.com>
2018-03-29 15:37:13 -0700

    Bug #11 - add additional routes

--- src/quantum/routes.jsx
+++ src/quantum/routes.jsx
@@ -10,13 +10,20 @@ import Subbenchmark from './subbenchmarks';
 
 const routes = () => (
   <Switch>
-    <Route path='/quantum32bit' component={Quantum32bit} />
-    <Route path='/quantum64bit' component={Quantum64bit} />
-    <Route path='/quantum/responsiveness/parent' component={QuantumResponsivenessParent} />
-    <Route path='/quantum/responsiveness/content' component={QuantumResponsivenessContent} />
-    <Route path='/quantum/pageload/render' component={QuantumPageLoadRender} />
-    <Route path='/quantum/track' component={QuantumTracking} />
-    <Route path='/quantum/:platform/:suite' component={Subbenchmark} />
+    <Route path='/quantum32bit' exact component={Quantum32bit} />
+    <Route path='/quantum64bit' exact component={Quantum64bit} />
+
+    <Route path='/quantum32bit/responsiveness/parent' component={QuantumResponsivenessParent} />
+    <Route path='/quantum32bit/responsiveness/content' component={QuantumResponsivenessContent} />
+    <Route path='/quantum32bit/pageload/render' component={QuantumPageLoadRender} />
+    <Route path='/quantum32bit/track' component={QuantumTracking} />
+    <Route path='/quantum32bit/:platform/:suite' component={Subbenchmark} />
+
+    <Route path='/quantum64bit/responsiveness/parent' component={QuantumResponsivenessParent} />
+    <Route path='/quantum64bit/responsiveness/content' component={QuantumResponsivenessContent} />
+    <Route path='/quantum64bit/pageload/render' component={QuantumPageLoadRender} />
+    <Route path='/quantum64bit/track' component={QuantumTracking} />
+    <Route path='/quantum64bit/:platform/:suite' component={Subbenchmark} />
   </Switch>
 );
 
