05a54abc49c801a32b724be3bad3b422fd03e005
Armen Zambrano G <armenzg@gmail.com>
2018-06-13 06:47:37 -0700

    Add Sentry.io support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import Raven from 'raven-js';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import './index.css';
 import Routes from './routes';
+
+if (process.env.NODE_ENV === 'production') {
+  Raven.config('https://77916a47017347528d25824beb0a077e@sentry.io/1225660').install();
+}
 
 const root = document.getElementById('root');
 const load = () => render(
