2b29142a53a96c50850a5225860cd6dc19718218
Armen Zambrano G <armenzg@gmail.com>
2018-06-18 13:28:10 -0400

    refactor: Move Android sites into a configuration file

--- src/android/android-index.js
+++ src/android/android-index.js
@@ -7,7 +7,7 @@ import cx from 'classnames';
 
 import Dashboard from '../dashboard';
 import GraphContainer from '../components/graph-container';
-import { klarTargets, graphProps } from './constants';
+import config from './constants';
 import GenericErrorBoundary from '../components/genericErrorBoundary';
 
 export default class AndroidIndex extends React.Component {
@@ -19,104 +19,18 @@ export default class AndroidIndex extends React.Component {
         className={cx('summary')}
       >
         <div className='row'>
-          <GenericErrorBoundary>
-            <GraphContainer
-              query={{ site: 'abcnews.go.com' }}
-              title='abcnews.go.com'
-              legend={graphProps.legend}
-              baselines={[{ value: klarTargets['abcnews.go.com'], label: '+20%' }]}
-              target={graphProps.target}
-              api={graphProps.api}
-              keys={graphProps.keys}
-              width={graphProps.width}
-              height={graphProps.height}
-              checkStatus
-              targetValue={klarTargets['abcnews.go.com']}
-              targetLine='klar'
-            />
-          </GenericErrorBoundary>
-          <GenericErrorBoundary>
-            <GraphContainer
-              query={{ site: 'wikia-fandom' }}
-              title='wikia/fandom'
-              legend={graphProps.legend}
-              baselines={[{ value: klarTargets['wikia-fandom'], label: '+20%' }]}
-              target={graphProps.target}
-              api={graphProps.api}
-              keys={graphProps.keys}
-              width={graphProps.width}
-              height={graphProps.height}
-              checkStatus
-              targetValue={klarTargets['wikia-fandom']}
-              targetLine='klar'
-            />
-          </GenericErrorBoundary>
-          <GenericErrorBoundary>
-            <GraphContainer
-              query={{ site: 'buzzfeed' }}
-              title='buzzfeed'
-              legend={graphProps.legend}
-              baselines={[{ value: klarTargets.buzzfeed, label: '+20%' }]}
-              target={graphProps.target}
-              api={graphProps.api}
-              keys={graphProps.keys}
-              width={graphProps.width}
-              height={graphProps.height}
-              checkStatus
-              targetValue={klarTargets.buzzfeed}
-              targetLine='klar'
-            />
-          </GenericErrorBoundary>
+          {[config.abcnews, config.wikiaFandom, config.buzzfeed].map(siteConfig => (
+            <GenericErrorBoundary>
+              <GraphContainer {...siteConfig} />
+            </GenericErrorBoundary>
+          ))}
         </div>
         <div className='row'>
-          <GenericErrorBoundary>
-            <GraphContainer
-              query={{ site: 'yelp.de' }}
-              title='yelp.de'
-              legend={graphProps.legend}
-              baselines={[{ value: klarTargets['yelp.de'], label: '+20%' }]}
-              target={graphProps.target}
-              api={graphProps.api}
-              keys={graphProps.keys}
-              width={graphProps.width}
-              height={graphProps.height}
-              checkStatus
-              targetValue={klarTargets['yelp.de']}
-              targetLine='klar'
-            />
-          </GenericErrorBoundary>
-          <GenericErrorBoundary>
-            <GraphContainer
-              query={{ site: 'eurosport.eu' }}
-              title='eurosport.eu'
-              legend={graphProps.legend}
-              baselines={[{ value: klarTargets['eurosport.eu'], label: '+20%' }]}
-              target={graphProps.target}
-              api={graphProps.api}
-              keys={graphProps.keys}
-              width={graphProps.width}
-              height={graphProps.height}
-              checkStatus
-              targetValue={klarTargets['eurosport.eu']}
-              targetLine='klar'
-            />
-          </GenericErrorBoundary>
-          <GenericErrorBoundary>
-            <GraphContainer
-              query={{ site: 'm.ranker.com' }}
-              title='m.ranker.com'
-              legend={graphProps.legend}
-              baselines={[{ value: klarTargets['m.ranker.com'], label: '+20%' }]}
-              target={graphProps.target}
-              api={graphProps.api}
-              keys={graphProps.keys}
-              width={graphProps.width}
-              height={graphProps.height}
-              checkStatus
-              targetValue={klarTargets['m.ranker.com']}
-              targetLine='klar'
-            />
-          </GenericErrorBoundary>
+          {[config.yelp, config.eurosport, config.ranker].map(siteConfig => (
+            <GenericErrorBoundary>
+              <GraphContainer {...siteConfig} />
+            </GenericErrorBoundary>
+          ))}
         </div>
       </Dashboard>
     );
--- src/android/constants.js
+++ src/android/constants.js
@@ -1,17 +1,66 @@
-export const klarTargets = {
-  'abcnews.go.com': 7.036363636,
-  'wikia-fandom': 4.461818182,
+const klarTargets = {
+  abcnews: 7.036363636,
+  wikiaFandom: 4.461818182,
   buzzfeed: 6.087272727,
-  'yelp.de': 5.836363636,
-  'eurosport.eu': 8.236363636,
-  'm.ranker.com': 3.610909091,
+  yelp: 5.836363636,
+  eurosport: 8.236363636,
+  ranker: 3.610909091,
 };
 
-export const graphProps = {
+const graphDefaults = {
   api: 'android/klar',
   legend: ['focus (WV)', 'klar (GV)'],
   target: 'GV within 20% of WV',
   width: 600,
   height: 300,
   keys: ['focus', 'klar'],
+  checkStatus: true,
+  targetLine: 'klar',
 };
+
+const configuration = {
+  abcnews: {
+    ...graphDefaults,
+    query: { site: 'abcnews.go.com' },
+    title: 'abcnews.go.com',
+    baselines: [{ value: klarTargets.abcnews, label: '+20%' }],
+    targetValue: klarTargets.abcnews,
+  },
+  wikiaFandom: {
+    ...graphDefaults,
+    query: { site: 'wikia-fandom' },
+    title: 'wikia/fandom',
+    baselines: [{ value: klarTargets.wikiaFandom, label: '+20%' }],
+    targetValue: klarTargets.wikiaFandom,
+  },
+  buzzfeed: {
+    ...graphDefaults,
+    query: { site: 'buzzfeed' },
+    title: 'buzzfeed',
+    baselines: [{ value: klarTargets.buzzfeed, label: '+20%' }],
+    targetValue: klarTargets.buzzfeed,
+  },
+  yelp: {
+    ...graphDefaults,
+    query: { site: 'yelp.de' },
+    title: 'yelp.de',
+    baselines: [{ value: klarTargets.yelp, label: '+20%' }],
+    targetValue: klarTargets.yelp,
+  },
+  eurosport: {
+    ...graphDefaults,
+    query: { site: 'eurosport.eu' },
+    title: 'eurosport.eu',
+    baselines: [{ value: klarTargets.eurosport, label: '+20%' }],
+    targetValue: klarTargets.eurosport,
+  },
+  ranker: {
+    ...graphDefaults,
+    query: { site: 'm.ranker.com' },
+    title: 'm.ranker.com',
+    baselines: [{ value: klarTargets.ranker, label: '+20%' }],
+    targetValue: klarTargets.ranker,
+  },
+};
+
+export default configuration;
