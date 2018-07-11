8857766dbf6a17e2895abef6d63e89c8ac824948
Armen Zambrano G <armenzg@gmail.com>
2018-07-11 08:52:41 -0400

    Change colour of widget to indicate meeting the target or not

--- src/components/NimbledroidWidget/index.jsx
+++ src/components/NimbledroidWidget/index.jsx
@@ -16,12 +16,15 @@ export default class NimbledroidWidget extends Component {
     const labels = Object.keys(profile.data);
     const data = labels.map(product => profile.data[product]);
     const target = this.props.targetRatio * profile.lastDataPoints.focus;
+    const status = profile.lastDataPoints.klar / profile.lastDataPoints.focus;
 
     return (
       <Widget
         // This classname is to undo what .criteria-widget:not:first-child sets
         className={'no-left-margin'}
         title={profile.title}
+        targetStatus={status < 1 ? 'pass' : 'fail'}
+        status={status < 1 ? 'green' : 'yellow'}
       >
         <div>
           <div className='legend' ref={ele => this.legendTarget = ele} />
