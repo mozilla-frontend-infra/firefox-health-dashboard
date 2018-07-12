6c9a39ef0dd7bf980ccc00cdb1094e380774b556
Armen Zambrano G <armenzg@gmail.com>
2018-07-12 14:50:34 -0400

    Support linear progress to Dashboard component
    When we're still loading data in the widgets within the page we can
    tell this to the user by showing the progress line.
    You can pass the `loading` property to the Dashboard component.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Raven from 'raven-js';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import deepOrange from '@material-ui/core/colors/deepOrange';
 import './index.css';
 import GenericErrorBoundary from './components/genericErrorBoundary';
 import Routes from './routes';
@@ -9,12 +11,19 @@ if (process.env.NODE_ENV === 'production') {
   Raven.config('https://77916a47017347528d25824beb0a077e@sentry.io/1225660').install();
 }
 
+const theme = createMuiTheme({
+  palette: {
+    primary: deepOrange,
+  },
+});
 const root = document.getElementById('root');
 const load = () => render(
   (
     <AppContainer>
       <GenericErrorBoundary critical>
-        <Routes />
+        <MuiThemeProvider theme={theme}>
+          <Routes />
+        </MuiThemeProvider>
       </GenericErrorBoundary>
     </AppContainer>
   ), root,
