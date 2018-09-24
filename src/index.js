f44efb43c9bb0c572c178f25bc98cc70dd72dbb0
Armen Zambrano G <armenzg@gmail.com>
2018-09-24 16:03:50 -0400

    Remove Mui theme provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Raven from 'raven-js';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import deepOrange from '@material-ui/core/colors/deepOrange';
 import './index.css';
 import GenericErrorBoundary from './components/genericErrorBoundary';
 import Routes from './routes';
@@ -11,19 +9,12 @@ if (process.env.NODE_ENV === 'production') {
   Raven.config('https://77916a47017347528d25824beb0a077e@sentry.io/1225660').install();
 }
 
-const theme = createMuiTheme({
-  palette: {
-    primary: deepOrange,
-  },
-});
 const root = document.getElementById('root');
 const load = () => render(
   (
     <AppContainer>
       <GenericErrorBoundary critical>
-        <MuiThemeProvider theme={theme}>
-          <Routes />
-        </MuiThemeProvider>
+        <Routes />
       </GenericErrorBoundary>
     </AppContainer>
   ), root,
