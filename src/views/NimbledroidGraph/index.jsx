0afa977b4a030fd5391a505232a578e4eb78c782
Armen Zambrano G <armenzg@gmail.com>
2018-10-09 09:53:09 -0400

    Plot NimbledroidGraph in its own view

---
+++ src/views/NimbledroidGraph/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+import DashboardPage from '../../components/DashboardPage';
+import Section from '../../components/Section';
+import NimbledroidProductVersions from '../../containers/NimbledroidProductVersions';
+import NimbledroidSiteDrilldown from '../../containers/NimbledroidSiteDrilldown';
+
+class NimbledroidGraph extends Component {
+  static propTypes = {
+    site: propTypes.string,
+    location: propTypes.shape({
+      search: propTypes.string,
+    }),
+  };
+
+  render() {
+    const products = [
+      'org.mozilla.klar',
+      'org.mozilla.focus',
+      'com.chrome.beta',
+    ];
+    const targetRatio = 1.2;
+    const site = this.props.location.search.replace('?site=', '');
+    return (
+      <DashboardPage title='Android' subtitle='Release criteria'>
+        <Section title='Nimbledroid' subtitle='GeckoView vs Chrome Beta'>
+          <NimbledroidProductVersions products={products} />
+          <NimbledroidSiteDrilldown
+            configuration={{
+            baseProduct: 'org.mozilla.focus',
+            compareProduct: 'com.chrome.beta',
+            products: products,
+            site: site,
+            targetRatio: targetRatio,
+            }}
+          />
+        </Section>
+      </DashboardPage>
+    );
+  }
+}
+
+export default NimbledroidGraph;
