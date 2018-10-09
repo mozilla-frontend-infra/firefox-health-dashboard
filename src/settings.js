dd4b73dabbc4f88a010c9469dd06b6003b1d2cc8
Armen Zambrano G <armenzg@gmail.com>
2018-10-09 13:38:41 -0400

    Load Android graphs colors from the settings module

--- src/settings.js
+++ src/settings.js
@@ -3,4 +3,7 @@ const SETTINGS = {
   firefoxReleases: 'https://product-details.mozilla.org/1.0/firefox_history_major_releases.json',
 };
 
+// This is less than ideal but it helps
+export const COLORS = ['#e55525', '#ffcd02', '#45a1ff'];
+
 export default SETTINGS;
