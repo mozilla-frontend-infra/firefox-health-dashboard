0452ad723772e4210a07795ed609c1d0aa071f95
Armen Zambrano G <armenzg@gmail.com>
2018-10-17 13:12:46 -0400

    Filter out old Nimbledroid sites

--- src/utils/nimbledroid/index.jsx
+++ src/utils/nimbledroid/index.jsx
@@ -60,13 +60,51 @@ const generateSitesSummary = (count, numSites) => (
   ]
 );
 
+const FILTERED_OUT_SITES = [
+  'http://m.spiegel.de',
+  'http://m.spiegel.de/netzwelt/web/firefox-quantum-so-schlaegt-sich-der-mozilla-browser-gegen-google-chrome-a-1178579.html',
+  'http://www.wikia.com/fandom',
+  'https://abcnews.go.com/US/nfl-owners-stand-players-trump-kneeling-rebuke/story?id=50060482',
+  'https://gizmodo.com',
+  'https://gizmodo.com/you-can-actually-do-something-good-with-those-eclipse-g-1798289914',
+  'https://lifehacker.com',
+  'https://lifehacker.com/how-to-turn-your-raspberry-pi-into-a-retro-game-console-498561192',
+  'https://m.imgur.com',
+  'https://m.imgur.com/gallery/IayLJ',
+  'https://mashable.com/2018/02/13/reels-guide-to-black-panther.amp',
+  'https://mic.com/stories/262/the-art-of-an-organized-refrigerator',
+  'https://mobile.nytimes.com/2017/08/21/science/solar-eclipse.html',
+  'https://mobile.twitter.com',
+  'https://rumble.com',
+  'https://www.buzzfeed.com',
+  'https://www.cnbc.com',
+  'https://www.cnbc.com/2015/05/21/american-diversity-cities-where-it-works.html',
+  'https://www.cosmopolitan.com/stamp/apple-cider-vinegar',
+  'https://www.expedia.com',
+  'https://www.flipkart.com',
+  'https://www.flipkart.com/search?q=moto%20g5%20plus&sid=tyy/4io&as=on&as-show=on&otracker=start&as-pos=1_1_ic_Moto%20G5',
+  'https://www.google.com/maps/place/Mozilla,+331+E+Evelyn+Ave,+Mountain+View,+CA+94041,+USA/@37.3873148,-122.0600095,17z/data=!4m2!3m1!1s0x808fba016663a837:0x1d64663c633a9e5?force=pwa',
+  'https://www.healthyway.com/content/brilliant-ways-to-use-common-baby-products-for-adults',
+  'https://www.nytimes.com',
+  'https://www.reddit.com',
+  'https://www.reddit.com/r/IAmA/comments/7eojwf/protect_net_neutrality_save_the_internet',
+  'https://www.theguardian.com/football/2018/may/14/premier-league-2017-18-review-signing-of-the-season',
+  'https://www.washingtonpost.com/graphics/2018/national/amp-stories/border-wall',
+  'https://www.wired.com/amp-stories/space-photos-of-the-week-111817',
+];
+
+const includeScenario = scenario => (
+  scenario.includes('http')
+  && !FILTERED_OUT_SITES.includes(scenario)
+);
+
 export const generateSitesTableContent = (
   nimbledroidData,
   { baseProduct, compareProduct, targetRatio },
   ) => {
   const { meta, scenarios } = nimbledroidData;
   const filteredScenarios = Object.keys(scenarios)
-    .filter(scenario => scenario.includes('http'))
+    .filter(scenario => includeScenario(scenario))
     .map(scenario => scenarios[scenario]);
   const packageIds = Object.keys(meta);
   const numSites = Object.keys(filteredScenarios).length;
