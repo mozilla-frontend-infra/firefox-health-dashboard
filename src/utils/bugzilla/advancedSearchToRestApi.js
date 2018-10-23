7071128431e46606d754e2c01fce003662a7132b
Armen Zambrano G <armenzg@gmail.com>
2018-10-23 11:01:56 -0400

    Issue #170 - Add GeckoView burndowns

---
+++ src/utils/bugzilla/advancedSearchToRestApi.js
@@ -0,0 +1,13 @@
+const TRANSFORM_FIELD = {
+    chfieldfrom: 'creation_time',
+};
+
+const advancedSearchToRestApi = parameters => (
+    Object.keys(parameters).reduce((result, key) => {
+        const newKey = TRANSFORM_FIELD[key] || key;
+        result[newKey] = parameters[key];
+        return result;
+    }, {})
+);
+
+export default advancedSearchToRestApi;
