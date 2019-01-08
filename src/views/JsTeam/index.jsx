4d8c8295f872370b06bd167aab93d9c665da8cd8
Armen Zambrano G <armenzg@gmail.com>
2019-01-07 20:21:31 -0500

    Register tooltip

--- src/views/JsTeam/index.jsx
+++ src/views/JsTeam/index.jsx
@@ -1,8 +1,58 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { Line } from 'react-chartjs-2';
 
+import registerTooltip from './registerTooltip';
 import DashboardPage from '../../components/DashboardPage';
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(85,192,192,0.4)',
+        borderColor: 'rgba(85,192,192,0.4)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rrgba(85,192,192,0.4)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(85,192,192,0.4)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: 'My second dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [66, 60, 82, 83, 58, 57, 43],
+      },
+    ],
+  };
 
 const styles = ({
     content: {
@@ -15,12 +65,17 @@ class JsTeam extends Component {
         classes: PropTypes.shape({}).isRequired,
     };
 
+    constructor(props) {
+        super(props);
+        registerTooltip();
+    }
+
     render() {
         const { classes } = this.props;
         return (
           <DashboardPage title='JavaScript Performance dashboard'>
             <div className={classes.content}>
-              Under construction. Welcome to the future home of arewefastyet.com!
+              <Line data={data} />
             </div>
           </DashboardPage>
         );
---
+++ src/views/JsTeam/registerTooltip.jsx
@@ -0,0 +1,57 @@
+import Chart from 'chart.js';
+
+// XXX: TODO: handle the case when we click on the same data point
+const handleOnClick = (_, items) => {
+    Chart.lastClickedDataPoint = null;
+    // Check if the click happened on empty chart space or data point
+    if (items.length > 0) {
+        // A Chart element has a reference to which chart it belongs to and what
+        // the current tooltip in view has a reference to the last active tooltip
+        // eslint-disable-next-line no-underscore-dangle
+        Chart.lastClickedDataPoint = items[0]._chart.tooltip._lastActive[0];
+        Chart.helpers.each(Chart.instances, (chartItem) => {
+            chartItem.update();
+        });
+    }
+};
+
+const registerPlugin = () => {
+    Chart.plugins.register({
+        id: 'tooltipPlugin',
+        beforeRender(chart) {
+          this.pluginTooltips = [];
+          // Process only if a data point was clicked
+          if (Chart.lastClickedDataPoint) {
+            // eslint-disable-next-line no-underscore-dangle
+            if (Chart.lastClickedDataPoint._chart.chart.id !== chart.id) {
+              return;
+            }
+            const { _datasetIndex, _index } = Chart.lastClickedDataPoint;
+
+            this.pluginTooltips.push(new Chart.Tooltip({
+              _chart: chart.chart,
+              _chartInstance: chart,
+              _data: chart.data,
+              _options: chart.options.tooltips,
+              // eslint-disable-next-line no-underscore-dangle
+              _active: [chart.getDatasetMeta(_datasetIndex).data[_index]],
+            }, chart));
+          }
+        },
+        afterDatasetsDraw(easing) {
+          Chart.helpers.each(this.pluginTooltips, (tooltip) => {
+            tooltip.initialize();
+            tooltip.update();
+            tooltip.pivot();
+            tooltip.transition(easing).draw();
+          });
+        },
+      });
+};
+
+const registerTooltipPlugin = () => {
+  registerPlugin();
+  Chart.defaults.global.onClick = handleOnClick;
+};
+
+export default registerTooltipPlugin;
