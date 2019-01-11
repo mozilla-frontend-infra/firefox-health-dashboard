1fe987332038308993b5331bea632975b710c6cb
Syeda Aimen Batool <aimenbatool72@gmail.com>
2019-01-11 20:04:41 +0500

    Issue #211 - Add persistent tooltip (#242)
    When you click on a datapoint for ChartJs based graphs you will have a persistent tooltip.
    Clicking anywhere else on the graph will take away the tooltip.
    
    Based on original work by https://github.com/asingh217
    
    The code:
    https://github.com/asingh217/firefox-performance-dashboard/commit/1274e01f5393b8d5c7c84778eb8b0218f151fbfd

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { AppContainer } from 'react-hot-loader';
 import './index.css';
 import GenericErrorBoundary from './components/genericErrorBoundary';
 import Routes from './routes';
+import registerTooltip from './utils/registerTooltip';
 
 require('typeface-roboto');
 
 if (process.env.NODE_ENV === 'production') {
   Raven.config('https://77916a47017347528d25824beb0a077e@sentry.io/1225660').install();
 }
+// handle sticky tooltip for all charts
+registerTooltip();
 
 const root = document.getElementById('root');
 const load = () => render(
