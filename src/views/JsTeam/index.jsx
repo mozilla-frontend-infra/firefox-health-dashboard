f52dce77a3ae77b1e270211f2df55ad53fc525d2
Armen Zambrano G <armenzg@gmail.com>
2019-01-14 10:30:51 -0500

    WIP Show Bz perf graphs

--- src/views/JsTeam/index.jsx
+++ src/views/JsTeam/index.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import DashboardPage from '../../components/DashboardPage';
+import BugzillaBurndown from '../../containers/BugzillaBurndown';
+import BugzillaUrlContainer from '../../containers/BugzillaUrlContainer';
 
 const styles = ({
     content: {
@@ -19,9 +21,39 @@ class JsTeam extends Component {
         const { classes } = this.props;
         return (
           <DashboardPage title='JavaScript Performance dashboard'>
-            <div className={classes.content}>
-              Under construction. Welcome to the future home of arewefastyet.com!
-            </div>
+            <BugzillaUrlContainer
+              includeBugCount
+              queries={[
+                {
+                    text: 'All Fx66 perf bugs',
+                    parameters: {
+                        o1: 'equals',
+                        v1: '1513237',
+                        f1: 'blocked',
+                        resolution: ['FIXED', 'VERIFIED'],
+                    },
+                },
+              ]}
+            />
+            <BugzillaBurndown
+              queries={[
+                {
+                    label: 'Open Fx66 perf bugs',
+                    parameters: {
+                        blocks: '1513237',
+                        resolution: '---',
+                    },
+                },
+                {
+                    label: 'Closed Fx66 perf bugs',
+                    parameters: {
+                        blocks: '1513237',
+                        resolution: ['FIXED', 'VERIFIED'],
+                    },
+                },
+              ]}
+              title='Firefox 66 performance issues'
+            />
           </DashboardPage>
         );
   }
