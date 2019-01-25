c2ea0e247f2a500258e0779fbdc8c0c9df5d0020
Syeda Aimen Batool <aimenbatool72@gmail.com>
2019-01-26 00:15:33 +0500

    Add job, hg link and make requested changes

--- src/utils/perfherder/chartJs/perfherderFormatter.js
+++ src/utils/perfherder/chartJs/perfherderFormatter.js
@@ -19,18 +19,43 @@ const generateInitialOptions = meta => {
           const tooltipData = [];
           let delta = 'n/a';
           let deltaPercentage = 'n/a';
+          let jobLink = '';
+          let hgLink = '';
+          let dataset = 'n/a';
+          let currentData = 'n/a';
           if (tooltipItems[0].index > 0) {
-            const currentData = data.datasets[tooltipItems[0].datasetIndex]
-              .data[tooltipItems[0].index].y;
-            const previousData = data.datasets[tooltipItems[0].datasetIndex]
-              .data[tooltipItems[0].index - 1].y;
+            dataset = data.datasets[tooltipItems[0].datasetIndex].data;
+
+            currentData = dataset[tooltipItems[0].index].y;
+            const previousData = dataset[tooltipItems[0].index - 1].y;
 
             delta = (currentData - previousData).toFixed(2);
             deltaPercentage = (((currentData - previousData) / previousData) * 100).toFixed(2);
+
+            const tooltipDataSet = series.data[tooltipItems[0].index];
+            // make job link against job id
+            jobLink = `https://treeherder.mozilla.org/#/jobs?repo=mozilla-central&revision=ca0f00593e38cdab54c3a990059bbf1150e77365&selectedJob=${tooltipDataSet.job_id}&group_state=expanded`;
+
+            // generate hg link
+            const pushId = tooltipDataSet.push_id;
+            const previousPushID = series.data[tooltipItems[0].index - 1].push_id;
+
+            const pushiIdLink = `https://treeherder.mozilla.org/api/project/mozilla-central/resultset/${pushId}/?format=json`;
+            const previousPushiIdLink = `https://treeherder.mozilla.org/api/project/mozilla-central/resultset/${previousPushID}/?format=json`;
+
+            // get push data against pushid
+            const pushIdData = fetchPushidData(pushiIdLink);
+            const previousPushIdData = fetchPushidData(previousPushiIdLink);
+
+            const recentRevision = pushIdData.revision;
+            const previousRevision = previousPushIdData.revision;
+
+            hgLink = `https://hg.mozilla.org/mozilla-central/pushloghtml?fromchange=${previousRevision}&tochange=${recentRevision}`;
           }
-          const indicator = `${data.datasets[tooltipItems[0].datasetIndex].data[tooltipItems[0].index].y} (${higherOrLower})`;
-          delta = `Δ ${delta} (${deltaPercentage}%)`;
-          tooltipData.push(indicator, delta);
+          const indicator = `${currentData} (${higherOrLower})`;
+          jobLink = `Job(${jobLink})`;
+          hgLink = `hg (${hgLink})`;
+          tooltipData.push(indicator, `Δ ${delta} (${deltaPercentage}%)`, jobLink, hgLink);
           return tooltipData;
         },
       },
