c094e2e3e54652820ab256c6f5ba22bfaf44704b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-02-02 17:02:32 -0500

    fix

--- src/quantum/index.js
+++ src/quantum/index.js
@@ -91,7 +91,7 @@ export default class QuantumIndex extends React.Component {
         title: 'Page Load tests (TP6)',
         more: `/quantum/tp6?bits=${bits}`,
         rows: frum(TP6_PAGES)
-          .filter({ bits: bits })
+          .where({ bits: bits })
           .groupBy('title')
           .map((series, title) => (
             <PerfherderGraphContainer
