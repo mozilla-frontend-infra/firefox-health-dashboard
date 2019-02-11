7ae2937511b3186eee01d82de6118fc1ec1de6c6
Syeda Aimen Batool <aimenbatool72@gmail.com>
2019-02-12 00:59:20 +0500

    enable linting rules (#301)
    Enabled a few linting rules and some rules are still disabled and mentioned in #293

--- test/components/graph-container.test.js
+++ test/components/graph-container.test.js
@@ -1,33 +1,36 @@
+/* eslint-disable jest/valid-expect */
 /* global describe, it */
-/* eslint-disable */
 import assert from 'assert';
 import { expect } from 'chai';
-import { transformGraphData, determineStatusColor } from '../../src/utils/helpers';
+import {
+  transformGraphData,
+  determineStatusColor,
+} from '../../src/utils/helpers';
 
 describe('transformGraphData', () => {
   it('should take an array of objects and transform it into many arrays of objects', () => {
-
     const keys = ['value1', 'value2'];
     const dataInput = [
       { value1: 3, value2: 8, date: '2018-05-03' },
       { value1: 7, value2: 13, date: '2018-05-02' },
       { value1: 9, value2: 5, date: '2018-05-01' },
     ];
-
     const expectedData = [
       [
         { value: 3, date: new Date('2018-05-03') },
         { value: 7, date: new Date('2018-05-02') },
         { value: 9, date: new Date('2018-05-01') },
-      ], [
+      ],
+      [
         { value: 8, date: new Date('2018-05-03') },
         { value: 13, date: new Date('2018-05-02') },
         { value: 5, date: new Date('2018-05-01') },
       ],
     ];
-
     const actual = transformGraphData(keys, dataInput);
-    assert(keys.length === Object.keys(dataInput[0]).length - 1); // date should be omitted from keys list
+
+    assert(keys.length === Object.keys(dataInput[0]).length - 1);
+    // date should be omitted from keys list
     expect(actual).to.eql(expectedData);
   });
 });
@@ -43,7 +46,6 @@ describe('determineStatusColor', () => {
       { value1: 7, value2: 13, date: '2018-05-03' },
       { value1: 9, value2: 5, date: '2018-05-02' },
     ];
-
     const actual = determineStatusColor(dataInput, targetLine, targetValue);
     const actual2 = determineStatusColor(dataInput, targetLine2, targetValue2);
 
