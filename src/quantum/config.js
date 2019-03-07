6f089f70b0d7ca8cab63e1c1110b3716902e7589
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-03-07 10:07:50 -0500

    fix benchmarks

--- src/quantum/config.js
+++ src/quantum/config.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 import { frum } from '../vendor/queryOps';
 import Data from '../vendor/Data';
 
@@ -8,59 +10,63 @@ const CONFIG = {
       secondLink: '/quantum/windows10-64/motionmark_htmlsuite',
       secondTitle: 'Breakdown',
       signatures: {
-        'windows10-64': 'b7794dcfc9a7522e7c1615e4a59b13ee86c0d7a6',
+        'windows10-64': '8f014459793e2e94c3244d5edeaada0452b0c627',
       },
+      framework: 10,
     },
     {
       title: 'MotionMark Animometer',
       secondLink: '/quantum/windows10-64/motionmark_animometer',
       secondTitle: 'Breakdown',
       signatures: {
-        'windows10-64': 'b34709c79b6de3d4e1a260a60da9717f20163d66',
+        'windows10-64': '9ad671fb568a5b3027af35b5d42fc6dd385f25ed',
       },
+      framework: 10,
     },
-    {
-      title: 'ARES6',
-      secondLink: '/quantum/windows10-64/ARES6',
-      secondTitle: 'Breakdown',
-      signatures: {
-        'windows10-64': '22a0985bd873492d70d49c335a21fd49a73c87cf',
-      },
-    },
-    {
-      title: 'JetStream',
-      secondLink: '/quantum/windows10-64/JetStream',
-      secondTitle: 'Breakdown',
-      signatures: {
-        'windows10-64': '799db0d801c7248924e93ca5a0b770020498da69',
-      },
-    },
+    // {
+    //   title: 'ARES6',
+    //   secondLink: '/quantum/windows10-64/ARES6',
+    //   secondTitle: 'Breakdown',
+    //   signatures: {
+    //     'windows10-64': '22a0985bd873492d70d49c335a21fd49a73c87cf',
+    //   },
+    // },
+    // {
+    //   title: 'JetStream',  //RUN ON JS BENCH
+    //   secondLink: '/quantum/windows10-64/JetStream',
+    //   secondTitle: 'Breakdown',
+    //   signatures: {
+    //     'windows10-64': '799db0d801c7248924e93ca5a0b770020498da69',
+    //   },
+    // },
   ],
   windows32Regression: [
     {
       title: 'MotionMark HTML suite',
       secondLink: '/quantum/windows7-32/motionmark_htmlsuite',
       secondTitle: 'Breakdown',
-      signatures: { 'windows7-32': 'f90f8f88d6397a66fb9a586ddf19064f41ee3ce3' },
+      signatures: { 'windows7-32': 'd1984855d038409797bbc8ad82c32489eb04cc23' },
+      framework: 10,
     },
     {
       title: 'MotionMark Animometer',
       secondLink: '/quantum/windows7-32/motionmark_animometer',
       secondTitle: 'Breakdown',
-      signatures: { 'windows7-32': '4bbe842bb4e492b5f546291db455660287d10d84' },
+      signatures: { 'windows7-32': '3d5a0a5e3c37f74770bdcb75bd46347be228495f' },
+      framework: 10,
     },
-    {
-      title: 'ARES6',
-      secondLink: '/quantum/windows7-32/ARES6',
-      secondTitle: 'Breakdown',
-      signatures: { 'windows7-32': '9585321f110f16414e272956c8892a38d03e7797' },
-    },
-    {
-      title: 'JetStream',
-      secondLink: '/quantum/windows7-32/JetStream',
-      secondTitle: 'Breakdown',
-      signatures: { 'windows7-32': '95535e0278558bd59ae324920ca0469278faadb4' },
-    },
+    // {
+    //   title: 'ARES6',
+    //   secondLink: '/quantum/windows7-32/ARES6',
+    //   secondTitle: 'Breakdown',
+    //   signatures: { 'windows7-32': '9585321f110f16414e272956c8892a38d03e7797' },
+    // },
+    // {
+    //   title: 'JetStream',  //RUN ON JS BENCH
+    //   secondLink: '/quantum/windows7-32/JetStream',
+    //   secondTitle: 'Breakdown',
+    //   signatures: { 'windows7-32': '95535e0278558bd59ae324920ca0469278faadb4' },
+    // },
   ],
 };
 const PLATFORMS = [
