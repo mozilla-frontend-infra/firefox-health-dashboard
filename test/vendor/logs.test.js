f3b9c013243bf2bde8f02b12cba00ccb0808162f
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-03-08 17:47:01 -0500

    add test

---
+++ test/vendor/logs.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+/* global describe, it */
+import { Log } from '../../src/vendor/logs';
+
+describe('logs', () => {
+  it('error', () => {
+    expect(() => Log.error('problem')).toThrow();
+  });
+
+  it('warning', () => {
+    let result = null;
+
+    try {
+      Log.error('problem');
+    } catch (e) {
+      result = e.toData();
+    }
+
+    expect(result.template).toEqual('problem');
+    expect(result.props).toEqual(null);
+    expect(result.cause).toEqual(null);
+    expect(result.trace[0].fileName.endsWith('errors.jsx')).toBe(true);
+  });
+});
