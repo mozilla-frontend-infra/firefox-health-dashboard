42ff38d3c109cadfed8d148e2cdd248477f94c54
BrunoDM2943 <bdm2943@gmail.com>
2019-04-29 21:55:41 -0300

    More tests

--- test/components/bugzilla.test.js
+++ test/components/bugzilla.test.js
@@ -3,21 +3,20 @@ import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import BugzillaQueryUrl from '../../src/components/BugzillaUrl';
 
-describe("<BugzillaQueryUrl />", () => {
+describe('<BugzillaQueryUrl />', () => {
   it('renders correctly', () => {
     const tree = renderer
       .create(
-          <BugzillaQueryUrl
-            bugCount={1}
-            url={"http://github.com"}
-            text={"test"}
-            classes={{
-              padding: "X",
-              text: "Y",
-              margin: "Z"
-            }}
-
-           />
+        <BugzillaQueryUrl
+          bugCount={1}
+          url="http://github.com"
+          text="test"
+          classes={{
+            padding: 'X',
+            text: 'Y',
+            margin: 'Z',
+          }}
+        />
       )
       .toJSON();
 
@@ -27,16 +26,15 @@ describe("<BugzillaQueryUrl />", () => {
   it('renders correctly without bugs', () => {
     const tree = renderer
       .create(
-          <BugzillaQueryUrl
-            url={"http://github.com"}
-            text={"test"}
-            classes={{
-              padding: "X",
-              text: "Y",
-              margin: "Z"
-            }}
-
-           />
+        <BugzillaQueryUrl
+          url="http://github.com"
+          text="test"
+          classes={{
+            padding: 'X',
+            text: 'Y',
+            margin: 'Z',
+          }}
+        />
       )
       .toJSON();
 
---
+++ test/components/linearIndeterminate.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import LinearIndeterminate from '../../src/components/LinearIndeterminate';
+
+describe('<LinearIndeterminate />', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <LinearIndeterminate
+          classes={{
+            root: 'X'
+          }}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
