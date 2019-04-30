1d4a05a40de6d1b808257faa08ebdba99d6144e5
BrunoDM2943 <bdm2943@gmail.com>
2019-04-29 21:47:29 -0300

    Bugzilla tests

---
+++ test/components/bugzilla.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import BugzillaQueryUrl from '../../src/components/BugzillaUrl';
+
+describe("<BugzillaQueryUrl />", () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+          <BugzillaQueryUrl
+            bugCount={1}
+            url={"http://github.com"}
+            text={"test"}
+            classes={{
+              padding: "X",
+              text: "Y",
+              margin: "Z"
+            }}
+
+           />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly without bugs', () => {
+    const tree = renderer
+      .create(
+          <BugzillaQueryUrl
+            url={"http://github.com"}
+            text={"test"}
+            classes={{
+              padding: "X",
+              text: "Y",
+              margin: "Z"
+            }}
+
+           />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
