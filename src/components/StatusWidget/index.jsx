d1112814511f32a721c0b835a7305d662225cda3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-05-06 14:12:30 -0400

    move code to single file

--- src/components/StatusWidget/index.jsx
+++ src/components/StatusWidget/index.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Title from './Title';
+
+const Title = ({ enrich, hyperlink, text, tooltip }) => {
+  const className = enrich ? 'enrich' : '';
+  const span = <span className={className}>{text}</span>;
+
+  return hyperlink ? (
+    <a href={hyperlink} title={tooltip || hyperlink}>
+      {span}
+    </a>
+  ) : (
+    span
+  );
+};
+
+Title.propTypes = {
+  enrich: PropTypes.bool,
+  hyperlink: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  tooltip: PropTypes.string,
+};
 
 const StatusWidget = ({
   children,
