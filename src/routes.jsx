d9d750210e9cbdfcd6a76aad3bc560c7bc4760c8
dane <dane@watcha.com>
2019-10-02 17:48:20 +0900

    use lazy, Suspense for code-splitting on routes #159

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import AndroidPage from './android';
-import NimbledroidGraphPage from './nimbledroid/NimbledroidGraphPage';
-import Quantum from './quantum/Quantum';
-import TP6 from './quantum/TP6';
-import TP6M from './android/TP6m';
-import Power from './power';
-import Playback from './playback/playback';
-import PlaybackDetails from './playback/details';
-import Subtests from './quantum/subtests';
 
-const NoMatch = () => <div>404</div>;
+const Home = lazy(() => import(/* webpackChunkName: "home" */ './Home'));
+const AndroidPage = lazy(() => import(/* webpackChunkName: "andriod" */'./android'));
+const NimbledroidGraphPage = lazy(() => import(/* webpackChunkName: "nimbledroid" */'./nimbledroid/NimbledroidGraphPage'));
+const Quantum = lazy(() => import(/* webpackChunkName: "quantum" */'./quantum/Quantum'));
+const TP6 = lazy(() => import(/* webpackChunkName: "quantum-tp6" */'./quantum/TP6'));
+const TP6M = lazy(() => import(/* webpackChunkName: "andriod-tp6m" */'./android/TP6m'));
+const Power = lazy(() => import(/* webpackChunkName: "power" */'./power'));
+const Playback = lazy(() => import(/* webpackChunkName: "playback-playback" */'./playback/playback'));
+const PlaybackDetails = lazy(() => import(/* webpackChunkName: "playback-details" */ './playback/details'));
+const Subtests = lazy(() => import(/* webpackChunkName: "quantum-subtests" */'./quantum/subtests'));
+const NoMatch = lazy(() => import(/* webpackChunkName: "not-found" */'./NotFound'));
+
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/quantum/tp6" component={TP6} />
-      <Route path="/quantum/tp6m" component={TP6M} />
-      <Route path="/android/tp6m" component={TP6M} />
-      <Route path="/android/graph" component={NimbledroidGraphPage} />
+    <Suspense fallback="">
+      <Switch>
+        <Route path="/quantum/tp6" component={TP6} />
+        <Route path="/quantum/tp6m" component={TP6M} />
+        <Route path="/android/tp6m" component={TP6M} />
+        <Route path="/android/graph" component={NimbledroidGraphPage} />
 
-      <Route path="/android" component={AndroidPage} />
-      <Route path="/power" component={Power} />
-      <Route path="/playback/details" component={PlaybackDetails} />
-      <Route path="/playback" component={Playback} />
-      <Route path="/quantum/subtests" component={Subtests} />
-      <Route path="/quantum/:bits" component={Quantum} />
-      <Route path="/" component={Home} />
-      <Route component={NoMatch} />
-    </Switch>
+        <Route path="/android" component={AndroidPage} />
+        <Route path="/power" component={Power} />
+        <Route path="/playback/details" component={PlaybackDetails} />
+        <Route path="/playback" component={Playback} />
+        <Route path="/quantum/subtests" component={Subtests} />
+        <Route path="/quantum/:bits" component={Quantum} />
+        <Route path="/" component={Home} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Suspense>
   </BrowserRouter>
 );
 
