070fc70858f754b22a04dec33c658e75ce585420
the6thm0nth <the6thm0nth@outlook.com>
2019-10-06 16:04:54 +0900

    update: remove unnecessary chunks

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,17 +2,18 @@ import React, { lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Home = lazy(() => import(/* webpackChunkName: "home" */ './Home'));
-const AndroidPage = lazy(() => import(/* webpackChunkName: "andriod" */'./android'));
-const NimbledroidGraphPage = lazy(() => import(/* webpackChunkName: "nimbledroid" */'./nimbledroid/NimbledroidGraphPage'));
-const Quantum = lazy(() => import(/* webpackChunkName: "quantum" */'./quantum/Quantum'));
-const TP6 = lazy(() => import(/* webpackChunkName: "quantum-tp6" */'./quantum/TP6'));
-const TP6M = lazy(() => import(/* webpackChunkName: "andriod-tp6m" */'./android/TP6m'));
-const Power = lazy(() => import(/* webpackChunkName: "power" */'./power'));
-const Playback = lazy(() => import(/* webpackChunkName: "playback-playback" */'./playback/playback'));
-const PlaybackDetails = lazy(() => import(/* webpackChunkName: "playback-details" */ './playback/details'));
-const Subtests = lazy(() => import(/* webpackChunkName: "quantum-subtests" */'./quantum/subtests'));
-const NoMatch = lazy(() => import(/* webpackChunkName: "not-found" */'./NotFound'));
+import Home from './Home';
+import Power from './power';
+import NoMatch from './NotFound';
+import Playback from './playback/playback';
+import PlaybackDetails from './playback/details';
+
+const AndroidPage = lazy(() => import(/* webpackChunkName: "andriod" */ './android'));
+const NimbledroidGraphPage = lazy(() => import(/* webpackChunkName: "nimbledroid" */ './nimbledroid/NimbledroidGraphPage'));
+const Quantum = lazy(() => import(/* webpackChunkName: "quantum" */ './quantum/Quantum'));
+const TP6 = lazy(() => import(/* webpackChunkName: "quantum-tp6" */ './quantum/TP6'));
+const TP6M = lazy(() => import(/* webpackChunkName: "andriod-tp6m" */ './android/TP6m'));
+const Subtests = lazy(() => import(/* webpackChunkName: "quantum-subtests" */ './quantum/subtests'));
 
 const Routes = () => (
   <BrowserRouter>
